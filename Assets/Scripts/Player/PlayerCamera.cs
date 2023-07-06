09886acec50274d340c3acad8770ba8f34208886
MrAcorn <42018678+MrAcorn@users.noreply.github.com>
2023-07-06 15:53:12 -0700

    Update PlayerCamera.cs
    "Fixed" make sure its on the camera and not the player. Will try it the other way around later.

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -6,13 +6,22 @@ public class PlayerCamera : MonoBehaviour {
     private Camera playerCam;
     public float speed = 1;
     public Vector3 velocity = Vector3.zero, targetPos;
+    public Transform player;
+    //attach player movement
+    public PlayerMovement playerMov;
 
     private void Awake() {
-        playerCam = Camera.main;
+        //playerCam = Camera.main;
     }
 
-    void LateUpdate() {
-        targetPos = new Vector3(transform.position.x, transform.position.y, playerCam.transform.position.z);
-        playerCam.transform.position = Vector3.SmoothDamp(playerCam.transform.position, targetPos, ref velocity, speed * Time.deltaTime);
+    void LateUpdate()
+    {
+        // So this part is to just keep Z constant to what ever it was set to without messing with it.
+        float prevZ = transform.position.z;
+
+        Vector3 smoothPos = transform.position + (player.transform.position - transform.position) * 0.5f *playerMov.speed * Time.deltaTime;
+        transform.position = Vector3.SmoothDamp(transform.position, smoothPos, ref velocity, Time.deltaTime);
+        //Put x back where it was
+        transform.position = new Vector3(transform.position.x, transform.position.y, prevZ);
     }
 }
