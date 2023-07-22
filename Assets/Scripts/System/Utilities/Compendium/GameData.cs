15565f104497fa6c57d903262ebeb15c25bc2b62
Brendan Sirls <70664862+Terradimer@users.noreply.github.com>
2023-07-22 02:51:53 -0700

    Small change to Compendium.Gamedata to allow for  Prefabs to be serialized

--- Assets/Scripts/System/Utilities/Compendium/GameData.cs
+++ Assets/Scripts/System/Utilities/Compendium/GameData.cs
@@ -3,17 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Compendium.GameData {
-    public class Prefabs : MonoBehaviour {
-        public static GameObject EnemyPrefab {get; private set;}
-        public static GameObject PlayerPrefab {get; private set;} 
-
-        [SerializeField]
-        private GameObject _enemyPF_;
-
-        [SerializeField]
-        private GameObject _playerPF_;
-    }
-
     public static class AreaMath {
         private static Vector2 RandomAlongCircleEdge(float rad, Vector2 origin) {
             return (Random.insideUnitCircle.normalized * rad) + origin;
